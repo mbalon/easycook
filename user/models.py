43df1cc8f0cc687b932190065ec99e751aae933e
mbalon <balonmateusz@gmail.com>
2022-03-28 21:49:28 +0200

    add comment model

--- user/models.py
+++ user/models.py
@@ -1,8 +1,15 @@
-from django.db.models import CASCADE, Model, OneToOneField, TextField
+from django.db.models import CASCADE, Model, OneToOneField, TextField, ForeignKey
 from django.contrib.auth.models import User
 
+from easycook.recipe.models import Recipe
 
 class Profile(Model):
     """ This class is used to create accounts on site"""
     user = OneToOneField(User, on_delete=CASCADE)
     biography = TextField(null=True)
+
+
+class Comment(Model):
+    user = ForeignKey(Profile)
+    recipe = ForeignKey(Recipe)
+    content = TextField()
