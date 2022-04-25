d836d5df6d672e048e5723a709ed80620b886fb9
mbalon <balonmateusz@gmail.com>
2022-04-25 20:45:42 +0200

    add static files to settings

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,5 @@ from .views import ProfileCreateView, ProfileDetailView
 
 urlpatterns = [
     path("new-profile/", ProfileCreateView.as_view(), name="profile_create"),
-    path("profile-view", ProfileDetailView.as_view(), name="profile_detail")
+    path("profile-view/", ProfileDetailView.as_view(), name="profile_detail")
 ]
